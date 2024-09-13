093a95bf47ea4a3e7274f5424c65d0c944f875a2
= <jonathanoktavianofrizzy@gmail.com>
2024-09-13 12:48:42 +0700

    add : logbook WEEK-3

--- experimental/structural-server/connection.php
+++ experimental/structural-server/connection.php
@@ -1,16 +1,19 @@
 <?php
-    // Declare the variables
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 $servername = "localhost";
 $username   = "root";
 $password   = "";
 $dbname     = "shms";
 
-    // Create a new mysqli instance
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Check the connection
-if ($conn->connect_error) {
-    die("Failed to connect to MySQL: " . $conn->connect_error);
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_errno) {
+        throw new Exception("Failed to connect to MySQL: " . $conn->connect_error);
+    }
+} catch (Exception $e) {
+    error_log($e->getMessage());
+    echo "Error: Unable to connect to the database.";
+    exit();
 }
-
 ?>
--- experimental/structural-server/server.php
+++ experimental/structural-server/server.php
@@ -1,29 +1,38 @@
 <?php
 include("connection.php");
 
-if (!empty($_POST)) {
-      // POST request DATA sensor 
-    $humidity    = $_POST["humidity"];
-    $temperature = $_POST["temperature"];
-    $accelX      = $_POST["accelX"];
-    $accelY      = $_POST["accelY"];
-    $accelZ      = $_POST["accelZ"];
-    $gyroX       = $_POST["gyroX"];
-    $gyroY       = $_POST["gyroY"];
-    $gyroZ       = $_POST["gyroZ"];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    if (isset($_POST["humidity"], $_POST["temperature"], $_POST["accelX"], $_POST["accelY"], $_POST["accelZ"], $_POST["gyroX"], $_POST["gyroY"], $_POST["gyroZ"])) {
+          // Sanitize inputs using htmlspecialchars
+        $humidity    = htmlspecialchars($_POST["humidity"], ENT_QUOTES, 'UTF-8');
+        $temperature = htmlspecialchars($_POST["temperature"], ENT_QUOTES, 'UTF-8');
+        $accelX      = htmlspecialchars($_POST["accelX"], ENT_QUOTES, 'UTF-8');
+        $accelY      = htmlspecialchars($_POST["accelY"], ENT_QUOTES, 'UTF-8');
+        $accelZ      = htmlspecialchars($_POST["accelZ"], ENT_QUOTES, 'UTF-8');
+        $gyroX       = htmlspecialchars($_POST["gyroX"], ENT_QUOTES, 'UTF-8');
+        $gyroY       = htmlspecialchars($_POST["gyroY"], ENT_QUOTES, 'UTF-8');
+        $gyroZ       = htmlspecialchars($_POST["gyroZ"], ENT_QUOTES, 'UTF-8');
 
-    $query = "INSERT INTO db (humidity, temperature, accelX, accelY, accelZ, gyroX, gyroY, gyroZ) 
-              VALUES ('$humidity', '$temperature', '$accelX', '$accelY', '$accelZ', '$gyroX', '$gyroY', '$gyroZ')";
-
-      // Re-check data output
-    if ($conn->query($query) === TRUE) {
-        echo "Berhasil menyimpan data ke tabel db";
+        if ($stmt = $conn->prepare("INSERT INTO db (humidity, temperature, accelX, accelY, accelZ, gyroX, gyroY, gyroZ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)")) {
+              // Bind parameters and execute
+            $stmt->bind_param("ssssssss", $humidity, $temperature, $accelX, $accelY, $accelZ, $gyroX, $gyroY, $gyroZ);
+            if ($stmt->execute()) {
+                echo "Data successfully saved to the 'db' table.";
+            } else {
+                error_log("Execute Error: " . $stmt->error);
+                echo "Error: Unable to save data.";
+            }
+            $stmt->close();
+        } else {
+            error_log("Prepare Error: " . $conn->error);
+            echo "Error: Unable to prepare the query.";
+        }
     } else {
-        echo "Error: " . $query . "<br>" . $conn->error;
+        echo "Error: Incomplete POST data.";
     }
-    $conn->close();
 } else {
-    echo "No POST data received.";
+    echo "Error: Only POST requests are accepted.";
 }
-?>
 
+$conn->close();
+?>
